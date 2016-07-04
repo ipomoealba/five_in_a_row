3359116e379334adcfc6ecdbe92772ccbc8b839f
oscar <oscar84922tw@gmail.com>
2016-07-04 21:18:08 +0800

    fuck you bug

--- misCamp/build-tmp/source/misCamp.java
+++ misCamp/build-tmp/source/misCamp.java
@@ -14,20 +14,28 @@ import java.io.IOException;
 
 public class misCamp extends PApplet {
 
+// \u5224\u65b7\u57f7\u5b50\u65b9
 boolean whoGetChess = true;
 
 // \u5132\u5b58\u68cb\u76e4\u5404\u4f4d\u7f6e\u984f\u8272
 int checkerboard[][] = new int[9][9];
 
+// \u8a08\u7b97\u9023\u7e8c\u68cb\u5b50\u6578
+int countCheerX = 0;  //  X\u8ef8
+int countCheerY = 0;  //  Y\u8ef8
+int countCheerS = 0;  //  \u659c\u5411
+
+// \u521d\u59cb\u8a2d\u5b9a
 public void setup() {
 
+  // \u756b\u5e03\u5927\u5c0f
   
+  // \u80cc\u666f\u8272
   background(217,170,116);
-  strokeWeight(3);
-  line(50, 50, 450 , 50);
 
 }
 
+// \u4e0d\u65b7\u66f4\u65b0
 public void draw() {
 
   // \u756b\u68cb\u76e4
@@ -40,72 +48,180 @@ public void draw() {
     line(50+50*i , 50,50+50*i,450);
   }
 
-  judge();
+  // fill(220,220,220);
+  // noStroke();
+  // rect(470, 50, 100, 70, 7);
+  // judge();
 }
 
 public void mousePressed(){
-  // \u68cb\u5b50\u4f4d\u7f6e
-  int targetX = -100;
-  int targetY = -100;
 
-  // \u68cb\u5b50\u984f\u8272
-  int blackChess = 0;
-  int whiteChess = 255;
-  // \u9ed1\u5b50\u5148\u884c
-  int chessColor = blackChess;
-
-  for(int i = 0; i < 10; i++ ){
-    if(mouseX > 75+50*(i-1) && mouseX <  75+50*i){
-      targetX = 50+50*i;
-    }
-    if(mouseY > 75+50*(i-1) && mouseY <  75+50*i){
-      targetY = 50+50*i;
-    }
-  }
-
-  // \u5224\u65b7\u57f7\u5b50\u65b9\u984f\u8272
-  if(whoGetChess){
-    chessColor = blackChess;
-    checkerboard[(targetX-50)/50][(targetY-50)/50] = 1;
+  if(mouseX > 470 || mouseY > 470){
+    // \u8d85\u51fa\u68cb\u76e4 \u7121\u53cd\u61c9
+  }else if (mouseY < 50 || mouseX < 50) {
+    // \u8d85\u51fa\u68cb\u76e4 \u7121\u53cd\u61c9
   }else{
-    chessColor = whiteChess;
-    checkerboard[(targetX-50)/50][(targetY-50)/50] = 2;
-  }
+    // \u5728\u68cb\u76e4\u5167
+    // \u68cb\u5b50\u4f4d\u7f6e
+    int targetX = -100;
+    int targetY = -100;
 
-  // \u5efa\u7acb\u4e00\u500b\u68cb\u5b50\u7684\u7269\u4ef6
-  // \u4e26\u8a2d\u5b9a\u5176\u5ea7\u6a19\u4ee5\u53ca\u984f\u8272
-  Chess newChess = new Chess(chessColor,targetX,targetY);
-  newChess.getChess();
+    // \u68cb\u5b50\u984f\u8272
+    int blackChess = 0;
+    int whiteChess = 255;
+
+    // \u9ed1\u5b50\u5148\u884c
+    int chessColor = blackChess;
 
 
-  // \u8b8a\u66f4\u57f7\u5b50\u8005\u984f\u8272
-  whoGetChess = !whoGetChess;
-  for (int[] o : checkerboard) {
-    println("o: "+o);
-  }
-  // println("checkerboard: "+checkerboard);
-}
+    for(int i = 0; i < 9; i++ ){
 
-public void judge(){
-  for(int i = 0;i< 4;i++){
-    for(int j = 0;j< 4;j++){
-      // \u8a08\u7b97\u9023\u7e8c\u68cb\u5b50\u6578
-      int countCheer = 0;
+      if(mouseX > 75+50*(i-1) && mouseX <  75+50*i){
+        targetX = 50+50*i;
+      }
+      if(mouseY > 75+50*(i-1) && mouseY <  75+50*i){
+        targetY = 50+50*i;
+      }
+    }
 
-      for(int k = 0;k<5;k++){
-        if(checkerboard[i][j] != 0){
-          if(checkerboard[i][j] == checkerboard[i+k][j]){
-            countCheer++;
-          }
-          if(countCheer>=5){
-            background(0);
-          }
+    // \u5224\u65b7\u57f7\u5b50\u65b9\u984f\u8272
+    if(checkerboard[(targetX-50)/50][(targetY-50)/50] == 0){
+
+      if(whoGetChess){
+        chessColor = blackChess;
+        checkerboard[(targetX-50)/50][(targetY-50)/50] = 1;
+      }else{
+        chessColor = whiteChess;
+        checkerboard[(targetX-50)/50][(targetY-50)/50] = 2;
+      }
+
+      // \u5efa\u7acb\u4e00\u500b\u68cb\u5b50\u7684\u7269\u4ef6
+      // \u4e26\u8a2d\u5b9a\u5176\u5ea7\u6a19\u4ee5\u53ca\u984f\u8272
+      Chess newChess = new Chess(chessColor,targetX,targetY);
+      newChess.getChess();
+
+      // \u8b8a\u66f4\u57f7\u5b50\u8005\u984f\u8272
+      whoGetChess = !whoGetChess;
+
+    }
+
+    judge();
+    // ================ \u6e2c\u8a66\u68cb\u76e4\u72c0\u614b =========================
+    for (int i = 0 ; i < 9; i++) {
+      for(int j = 0 ;j < 9; j++){
+        print("| "+checkerboard[j][i]);
+        if(j==8){
+          println(" |");
+          println("-------------------------------------");
+        }else{
+          print(" ");
         }
       }
     }
+    println("=====================================");
+    println("");
+    // ======================================================
+  }
+
+}
+
+int counter = 0;
+public void judge(){
+  // for(int i = 0;i<9;i++){
+    for(int i = 0 ;i < 9 ;i++){
+      compareX(0,i);
+      compareY(i,0);
+    }
+
+  // for(int i = 0;i< 9;i++){
+
+  //   for(int j = 0;j< 9;j++){
+
+  //     // for(int k = 0;k<5;k++){
+
+  //       if(checkerboard[i][j] != 0){
+
+  //         if (i-1 > 0 && j-1 > 0) {
+  //           if(i+1 < 9 && j+1 < 9){
+  //           if( checkerboard[i][j] == checkerboard[i-1][j]){
+  //             println("i: "+i);
+  //             println("checkerboard[i][j]: "+checkerboard[i][j]);
+  //             println("j: "+j);
+  //             println("checkerboard[i][j-1]: "+checkerboard[i][j-1]);
+  //             countCheerX++;
+  //           }
+
+  //         if( checkerboard[i][j] == checkerboard[i][j-k]){
+  //           countCheerY++;
+  //         }
+
+  //         }
+
+  //         println("countCheerX: "+countCheerX);
+  //         println("countCheerY: "+countCheerY);
+
+  //         if(countCheerX >=5 && countCheerY >= 5){
+  //           background(0);
+  //         }
+
+  //       }
+  //     }
+  //   // }
+
+  //   }
+  // }
+  countCheerX = 0;
+  countCheerY = 0;
+  countCheerS = 0;
+}
+
+public void compareX(int i , int j){
+  if(i < 8 && j < 8){
+
+      if(checkerboard[i][j] == checkerboard[i+1][j]){
+        if(checkerboard[i][j] != 0){
+            countCheerX++;
+
+        }
+        compareX(i+1,j);
+      }else {
+        println("countCheerX: "+countCheerX);
+        countCheerX = 0;
+        if(i+1 < 8){
+          compareX(i,j);
+        }
+      }
+      winner();
   }
 }
 
+public void compareY(int i , int j){
+  if(j < 8 && i < 8){
+      if(checkerboard[i][j+1] == checkerboard[i][j+1] ){
+         if(checkerboard[i][j] != 0){
+        println("countCheerY: "+countCheerY);
+          countCheerY++;
+          }
+          compareY(i,j+1);
+
+      }else{
+        println("countCheerY: "+countCheerY);
+        countCheerY = 0;
+
+        if(j+1 < 8){
+          compareY(i,j);
+        }
+      }
+      winner();
+  }
+
+}
+
+public void winner(){
+  if(countCheerX >=5 || countCheerY >= 5){
+     background(0);
+  }
+}
 class Chess{
   int chessColor = 0;
   int targetX = 0;
--- misCamp/misCamp.pde
+++ misCamp/misCamp.pde
@@ -1,17 +1,25 @@
+// 判斷執子方
 boolean whoGetChess = true;
 
 // 儲存棋盤各位置顏色
 int checkerboard[][] = new int[9][9];
 
+// 計算連續棋子數
+int countCheerX = 0;  //  X軸
+int countCheerY = 0;  //  Y軸
+int countCheerS = 0;  //  斜向
+
+// 初始設定
 void setup() {
 
+  // 畫布大小
   size( 600, 500 );
+  // 背景色
   background(217,170,116);
-  strokeWeight(3);
-  line(50, 50, 450 , 50);
 
 }
 
+// 不斷更新
 void draw() {
 
   // 畫棋盤
@@ -24,69 +32,177 @@ void draw() {
     line(50+50*i , 50,50+50*i,450);
   }
 
-  judge();
+  // fill(220,220,220);
+  // noStroke();
+  // rect(470, 50, 100, 70, 7);
+  // judge();
 }
 
 void mousePressed(){
-  // 棋子位置
-  int targetX = -100;
-  int targetY = -100;
 
-  // 棋子顏色
-  int blackChess = 0;
-  int whiteChess = 255;
-  // 黑子先行
-  int chessColor = blackChess;
-
-  for(int i = 0; i < 10; i++ ){
-    if(mouseX > 75+50*(i-1) && mouseX <  75+50*i){
-      targetX = 50+50*i;
-    }
-    if(mouseY > 75+50*(i-1) && mouseY <  75+50*i){
-      targetY = 50+50*i;
-    }
-  }
-
-  // 判斷執子方顏色
-  if(whoGetChess){
-    chessColor = blackChess;
-    checkerboard[(targetX-50)/50][(targetY-50)/50] = 1;
+  if(mouseX > 470 || mouseY > 470){
+    // 超出棋盤 無反應
+  }else if (mouseY < 50 || mouseX < 50) {
+    // 超出棋盤 無反應
   }else{
-    chessColor = whiteChess;
-    checkerboard[(targetX-50)/50][(targetY-50)/50] = 2;
-  }
+    // 在棋盤內
+    // 棋子位置
+    int targetX = -100;
+    int targetY = -100;
 
-  // 建立一個棋子的物件
-  // 並設定其座標以及顏色
-  Chess newChess = new Chess(chessColor,targetX,targetY);
-  newChess.getChess();
+    // 棋子顏色
+    int blackChess = 0;
+    int whiteChess = 255;
+
+    // 黑子先行
+    int chessColor = blackChess;
 
 
-  // 變更執子者顏色
-  whoGetChess = !whoGetChess;
-  for (int[] o : checkerboard) {
-    println("o: "+o);
-  }
-  // println("checkerboard: "+checkerboard);
-}
+    for(int i = 0; i < 9; i++ ){
 
-void judge(){
-  for(int i = 0;i< 4;i++){
-    for(int j = 0;j< 4;j++){
-      // 計算連續棋子數
-      int countCheer = 0;
+      if(mouseX > 75+50*(i-1) && mouseX <  75+50*i){
+        targetX = 50+50*i;
+      }
+      if(mouseY > 75+50*(i-1) && mouseY <  75+50*i){
+        targetY = 50+50*i;
+      }
+    }
 
-      for(int k = 0;k<5;k++){
-        if(checkerboard[i][j] != 0){
-          if(checkerboard[i][j] == checkerboard[i+k][j]){
-            countCheer++;
-          }
-          if(countCheer>=5){
-            background(0);
-          }
+    // 判斷執子方顏色
+    if(checkerboard[(targetX-50)/50][(targetY-50)/50] == 0){
+
+      if(whoGetChess){
+        chessColor = blackChess;
+        checkerboard[(targetX-50)/50][(targetY-50)/50] = 1;
+      }else{
+        chessColor = whiteChess;
+        checkerboard[(targetX-50)/50][(targetY-50)/50] = 2;
+      }
+
+      // 建立一個棋子的物件
+      // 並設定其座標以及顏色
+      Chess newChess = new Chess(chessColor,targetX,targetY);
+      newChess.getChess();
+
+      // 變更執子者顏色
+      whoGetChess = !whoGetChess;
+
+    }
+
+    judge();
+    // ================ 測試棋盤狀態 =========================
+    for (int i = 0 ; i < 9; i++) {
+      for(int j = 0 ;j < 9; j++){
+        print("| "+checkerboard[j][i]);
+        if(j==8){
+          println(" |");
+          println("-------------------------------------");
+        }else{
+          print(" ");
         }
       }
     }
+    println("=====================================");
+    println("");
+    // ======================================================
+  }
+
+}
+
+int counter = 0;
+void judge(){
+  // for(int i = 0;i<9;i++){
+    for(int i = 0 ;i < 9 ;i++){
+      compareX(0,i);
+      compareY(i,0);
+    }
+
+  // for(int i = 0;i< 9;i++){
+
+  //   for(int j = 0;j< 9;j++){
+
+  //     // for(int k = 0;k<5;k++){
+
+  //       if(checkerboard[i][j] != 0){
+
+  //         if (i-1 > 0 && j-1 > 0) {
+  //           if(i+1 < 9 && j+1 < 9){
+  //           if( checkerboard[i][j] == checkerboard[i-1][j]){
+  //             println("i: "+i);
+  //             println("checkerboard[i][j]: "+checkerboard[i][j]);
+  //             println("j: "+j);
+  //             println("checkerboard[i][j-1]: "+checkerboard[i][j-1]);
+  //             countCheerX++;
+  //           }
+
+  //         if( checkerboard[i][j] == checkerboard[i][j-k]){
+  //           countCheerY++;
+  //         }
+
+  //         }
+
+  //         println("countCheerX: "+countCheerX);
+  //         println("countCheerY: "+countCheerY);
+
+  //         if(countCheerX >=5 && countCheerY >= 5){
+  //           background(0);
+  //         }
+
+  //       }
+  //     }
+  //   // }
+
+  //   }
+  // }
+  countCheerX = 0;
+  countCheerY = 0;
+  countCheerS = 0;
+}
+
+void compareX(int i , int j){
+  if(i < 8 && j < 8){
+
+      if(checkerboard[i][j] == checkerboard[i+1][j]){
+        if(checkerboard[i][j] != 0){
+            countCheerX++;
+
+        }
+        compareX(i+1,j);
+      }else {
+        println("countCheerX: "+countCheerX);
+        countCheerX = 0;
+        if(i+1 < 8){
+          compareX(i,j);
+        }
+      }
+      winner();
   }
 }
 
+void compareY(int i , int j){
+  if(j < 8 && i < 8){
+      if(checkerboard[i][j+1] == checkerboard[i][j+1] ){
+         if(checkerboard[i][j] != 0){
+        println("countCheerY: "+countCheerY);
+          countCheerY++;
+          }
+          compareY(i,j+1);
+
+      }else{
+        println("countCheerY: "+countCheerY);
+        countCheerY = 0;
+
+        if(j+1 < 8){
+          compareY(i,j);
+        }
+      }
+      winner();
+  }
+
+}
+
+void winner(){
+  if(countCheerX >=5 || countCheerY >= 5){
+     background(0);
+  }
+}
